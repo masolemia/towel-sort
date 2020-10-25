14282bae4f75e50235e01494de6db97ea04666c0
Masha <mariasoldatenko@mail.ru>
2020-10-25 20:23:22 +0300

    feat:soluton

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (m) {
-    if(!m.length) return [];
-    m.map(a => {if(m.indexOf(a) % 2) a.reverse()});
-    return m.flat();
+module.exports = function towelSort (matrix) {
+    let outArraya = [];
+    if(matrix === undefined){
+        return [];
+      }
+    matrix.forEach((element, i)=> {
+      if(i%2 === 0){
+        outArraya.push(...element.sort((a,b)=>a-b));
+      }else{
+        outArraya.push(...element.sort((a,b)=>b-a));
+      }
+    });
+    return outArraya;
 }
